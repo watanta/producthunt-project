a687e7f345a07fbb20ca0274457886ab6a76e685
EC2 Default User <ec2-user@ip-172-31-15-67.us-west-2.compute.internal>
2018-07-10 13:19:40 +0000

    Complete Udemy

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from products import views
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', views.home, name="home")
-]
+    path('products/', include('products.urls')),
+    path('', views.home, name="home"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
